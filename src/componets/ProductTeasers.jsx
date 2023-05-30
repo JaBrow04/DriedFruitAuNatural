5bd2bf0e9f9e9ee457a7da95f389203727e26b21
JaBrow04 <115672295+JaBrow04@users.noreply.github.com>
2023-05-30 15:53:31 -0500

    redirect

--- src/componets/ProductTeasers.jsx
+++ src/componets/ProductTeasers.jsx
@@ -12,7 +12,7 @@ import {
 } from "./ProductTeaser.style";
 
 const ProductTeasers =(
-    {direction, imageName}
+    { imageName}
 ) =>
 {
     let navigate = useNavigate();
@@ -27,7 +27,7 @@ const ProductTeasers =(
                 <EmptyBottomBoxes/>
                 <TextBoxStyled>
                     <h1>The Difference</h1>
-                    <p>Locally sourced produce dehydrated in the heart of Sioux Falls, South Dakota.</p>
+                    <p>Locally dehydrated produce in the heart of Sioux Falls, South Dakota.</p>
                     <Button onClick={routeChange}>Discover</Button>
                 </TextBoxStyled>
 
